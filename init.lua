d8fe787c833f8f15e389024d6d563e94f4839088
bmcApm <bmc.apm@gmail.com>
2016-02-16 14:39:46 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,11 +6,11 @@
 -- To change this template use File | Settings | File Templates.
 --
 
---local _helper = require('pluginHelper')
+local _helper = require('pluginHelper')
 --local _windowsOS = require('windowsOS')
 --local _macOS = require('macOS')
 --local _linuxOS = require('linuxOS')
-local framework = require('framework')
+--__local framework = require('framework')
 -- local _zip  = require('zip')
 
 
@@ -18,4 +18,4 @@ local framework = require('framework')
 --if (_helper.getOsName()) == "windows" then _windowsOS.execute() end
 --if (_helper.getOsName()) == "linux" then _macOS.execute() end
 --if (_helper.getOsName()) == "mac" then _linuxOS.execute() end
-print('done')
+print(_helper.test())
